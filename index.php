02d40a8818b10ca7775fcea2ea8e81ef4eb62764
Junior Boaventura <junior.boaventura1@gmail.com>
2015-03-06 09:36:45 +0100

    Projet rush terminé

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="icon" type="image/png" href="img/spot-logo-app.png">
 	<script src="js/vendor/jquery-1.7.2.min.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Roboto:500,400italic,100,300,700,500italic,100italic,300italic,400' rel='stylesheet' type='text/css'>
 	<script src="js/plugins.js"></script>
@@ -46,6 +47,13 @@
 				});
 			}
 
+			if(num == 2){
+				$('.iphone').css({
+					"-webkit-transform": "rotate(-90deg)"
+
+				});
+
+			}
 			$(".slider").css({
 				"transform" : "translateY("+position[num]+"px)",
 				"-webkit-transform": "translateY("+position[num]+"px)",
@@ -76,7 +84,7 @@
 			<ul>
 				<li><a href="#section0">Home</a></li>
 				<li><a href="#section1">App</a></li>
-				<li><a href="#section2">Vidéo</a></li>
+				<li><a href="#section2">Vidéos</a></li>
 				<li><a href="#section3">Partage</a></li>
 				<li><a href="#section4">Premium</a></li>
 				<li><a href="#section5">Témoignages</a></li>
@@ -93,31 +101,34 @@
 					<div class="screen">
 						<div class="slider">
 
-							<img id="video0" src="pictures/white.jpg">
+							<img id="video0" src="pictures/white.jpg" alt="fond blanc">
 
-							<video id="video1" class="video-iphone" pause loop muted>
+							<video id="video1" class="video-iphone" loop muted>
 								<source src="movies/animation1.mp4" type="video/mp4" />
 								La vidéo ne peut être affichée
 							</video>
 
-							<img id="video2" src="pictures/white.jpg"> <!-- Video -->
-
-							<video id="video3" class="video-iphone" pause preload loop muted>
-								<source src="movies/test.mov"/>
+							<video id="video2" class="video-iphone" preload loop muted>
+								<source src="movies/animation4.mp4" type="video/mp4"/>
 								La vidéo ne peut être affichée
 							</video>
 
-							<video id="video4" class="video-iphone" pause preload loop muted>
+							<video id="video3" class="video-iphone" preload loop muted>
+								<source src="movies/animation5.mp4" type="video/mp4"/>
+								La vidéo ne peut être affichée
+							</video>
+
+							<video id="video4" class="video-iphone" preload loop muted>
 								<source src="movies/animation2.mp4" type="video/mp4"/>
 								La vidéo ne peut être affichée
 							</video>
 
-							<video id="video5" class="video-iphone" pause preload loop muted>
+							<video id="video5" class="video-iphone" preload loop muted>
 								<source src="movies/animation3.mp4" type="video/mp4"/>
 								La vidéo ne peut être affichée
 							</video>
 
-							<img id="video6" src="pictures/app-store.jpg">
+							<img id="video6" src="pictures/app-store.jpg" alt="Apple Store">
 
 						</div>
 					</div>
@@ -145,11 +156,11 @@
 	</section>
 
 	<section id="section1" class="scrollsections" >
-		<div class="background" style=""></div>
+		<div class="background"></div>
 		<table><tr><td>
 			<div class="wrap-content">
 				<div class="left" style="text-align:center; margin-top:100px;">
-					<img src="img/spot-logo-app.png" height="90" alt="" style="diplay:inline-block;">
+					<img src="img/spot-logo-app.png" height="90" style="diplay:inline-block;" alt="Logo App">
 				</div>
 				<div class="right">
 					<h2>Application</h2>
@@ -160,13 +171,13 @@
 	</section>
 
 	<section id="section2" class="scrollsections" >
-		<div class="background" style=""></div>
+		<div class="background"></div>
 		<table><tr><td>
 			<div class="wrap-content">
 				<div class="left">
-					<h2>Vidéo</h2>
+					<h2>Vidéos</h2>
 					<p>
-						<b>Visionnez</b> vos Spots pendant votre session, ou après. Libre à vous d’améliorer vos performances ou de choisir un meilleur angle de vue !
+						<b>Visionnez</b> vos Spots pendant votre session, <br>ou après. Libre à vous d’améliorer vos <br>performances ou de choisir un meilleur <br> angle de vue !
 					</p>
 				</div>
 			</div>
@@ -174,7 +185,7 @@
 	</section>
 
 	<section id="section3" class="scrollsections" >
-		<div class="background" style=""></div>
+		<div class="background"></div>
 		<table><tr><td>
 			<div class="wrap-content">
 				<div class="right">
@@ -188,7 +199,7 @@
 	</section>
 
 	<section id="section4" class="scrollsections" >
-		<div class="background" style=""></div>
+		<div class="background"></div>
 		<table><tr><td>
 			<div class="wrap-content">
 				<div class="left">
@@ -202,7 +213,7 @@
 	</section>
 
 	<section id="section5" class="scrollsections" >
-		<div class="background" style=""></div>
+		<div class="background"></div>
 		<table><tr><td>
 			<div class="wrap-content">
 				<div class="right">
@@ -216,15 +227,16 @@
 	</section>
 
 	<section id="section6" class="scrollsections" >
-		<div class="background" style=""></div>
+		<div class="background"></div>
 		<table><tr><td>
 			<div class="wrap-content">
 				<div class="left">
 					<h2>Télécharger</h2>
 					<p><b>Notre application</b> est disponible sur l’AppStore et Google Play.</p>
+					<a href="#" class="order">Commander</a>
 				</div>
 				<div class="right">
-					<img src="img/appstore-google-play.png" height="110" style="margin-top:100px;">
+					<img src="img/appstore-google-play.png" height="110" style="margin-top:100px;" alt="logo store">
 				</div>
 			</div>
 		</td></tr></table>
